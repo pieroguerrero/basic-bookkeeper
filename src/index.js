e0c934e13b82175b9c88b1dfec17acacb154f2fa
Piero <piero.guerrero@gmail.com>
2022-05-26 15:33:59 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route
+          path="/"
+          element={
+            <>
+              Hellow!!!!!\n Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\nHellow!!!!!\n
+              Hellow!!!!!\nHellow!!!!!\nHellow!!!!!\n{" "}
+            </>
+          }
+        />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
