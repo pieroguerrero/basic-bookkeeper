d6825f4321c3bde00800c2e956fe2283929b2c32
Piero <piero.guerrero@gmail.com>
2022-05-26 21:25:05 -0500

    completeed

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,7 +1,53 @@
+import { Link, NavLink, Outlet, useSearchParams } from "react-router-dom";
+import { getInvoices } from "../data";
+import { QueryNavLink } from "./QueryNavlink";
+
 export default function Invoices() {
+  let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
-    </main>
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <QueryNavLink
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "",
+                };
+              }}
+              to={"/invoices/" + invoice.number}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </QueryNavLink>
+          ))}
+      </nav>
+      <Outlet />
+    </div>
   );
 }
